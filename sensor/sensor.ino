4065b6d7b683c33bba999ae2206dd84dbb97c0df
yukinouee <yuki2023@suou.waseda.jp>
2024-03-15 19:53:07 +0900

    added rest.

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -5,6 +5,15 @@
 #define TRIG_PIN 10
 #define ECHO_PIN 11
 #define BUZ_PIN 12
+#define WHOLE 120000
+#define HALF 60000
+#define QUARTER 30000
+#define EIGHTH 15000
+#define SIXTEENTH 7500
+#define HALF_P 90000
+#define QUARTER_P 45000
+#define EIGHTH_P 22500
+#define SIXTEENTH_P 11200
 
 // 4 octave
 #define C4 261.63
@@ -78,223 +87,86 @@ char songlist[NUM_SONGS][16] = {
                                };
 
 void famima() {
-  int chord[11] = { Fs5, D5, A4, D5, E5, A5, E5, Fs5, E5, A4, D5 };
-  int i;
+  int bpm = 80;
+  int chord[12] = { Fs5, D5, A4, D5, E5, A5, 0, E5, Fs5, E5, A4, D5};
+  int duration[12] = {QUARTER/bpm, QUARTER/bpm, QUARTER/bpm, QUARTER/bpm, QUARTER/bpm, QUARTER_P/bpm, QUARTER_P/bpm, QUARTER/bpm, QUARTER/bpm, QUARTER/bpm, QUARTER/bpm, HALF/bpm};
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print(songlist[0]);
-  for(i=0;i < 5;i++){
-    tone(BUZ_PIN, chord[i]);
-    delay(300);
-    noTone(BUZ_PIN);
-    delay(100);
+  for(int i=0;i < 12;i++){
+    if(chord[i] == 0){
+      noTone(BUZ_PIN);
+      delay(duration[i]);
+    }else{
+      tone(BUZ_PIN, chord[i]);
+      delay(duration[i]);
+    }
   }
-  tone(BUZ_PIN, chord[5]);
-  delay(800);
   noTone(BUZ_PIN);
-  delay(400);
-  for(int j=i+1;j<10;j++){
-    tone(BUZ_PIN, chord[j]);
-    delay(300);
-    noTone(BUZ_PIN);
-    delay(100);
-  }
-  tone(BUZ_PIN, chord[10]);
-  delay(800);
-  noTone(BUZ_PIN);
-  delay(400);
 }
 
 void noritz() {
-  int chord[18] = { G5, F5, E5, G5, C6, B5, G5, D6, C6, E6, C6, B5, A5, F6, D6, C6, B5, C6 };
-  int i,j,k,l,m;
+  int bpm = 110;
+  int chord[23] = { G5,F5, E5, 0, G5, C6, B5, 0, G5, D6, C6, E6, 0, C6, B5, A5, 0, F6, D6, C6, 0, B5, C6};
+  int duration[23] = { QUARTER/bpm, QUARTER/bpm, QUARTER_P/bpm, EIGHTH/bpm, QUARTER/bpm, QUARTER/bpm, QUARTER_P/bpm, EIGHTH/bpm, QUARTER/bpm, QUARTER/bpm, HALF/bpm, HALF/bpm, HALF/bpm, QUARTER/bpm, QUARTER/bpm, QUARTER_P/bpm, EIGHTH/bpm, QUARTER/bpm, QUARTER/bpm, QUARTER_P/bpm, EIGHTH/bpm, HALF/bpm, HALF/bpm};
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print(songlist[1]);
-  for(i=0;i < 2;i++){
-    tone(BUZ_PIN, chord[i]);
-    delay(200);
-    noTone(BUZ_PIN);
-    delay(80);
+  for(int i=0;i < 23;i++){
+    if(chord[i] == 0){
+      noTone(BUZ_PIN);
+      delay(duration[i]);
+    }else{
+      tone(BUZ_PIN, chord[i]);
+      delay(duration[i]);
+    }
   }
-  tone(BUZ_PIN, chord[i]);
-  delay(300);
   noTone(BUZ_PIN);
-  delay(150);
-  for(j=i+1;j < 5;j++){
-    tone(BUZ_PIN, chord[j]);
-    delay(200);
-    noTone(BUZ_PIN);
-    delay(80);
-  }
-  tone(BUZ_PIN, chord[j]);
-  delay(300);
-  noTone(BUZ_PIN);
-  delay(150);
-  for(k=j+1;k < 8;k++){
-    tone(BUZ_PIN, chord[k]);
-    delay(200);
-    noTone(BUZ_PIN);
-    delay(80);
-  }
-  tone(BUZ_PIN, chord[k]);
-  delay(300);
-  noTone(BUZ_PIN);
-  delay(180);
-  tone(BUZ_PIN, chord[k+1]);
-  delay(600);
-  noTone(BUZ_PIN);
-  delay(500);
-
-  for(l=k+2;l < 12;l++){
-    tone(BUZ_PIN, chord[l]);
-    delay(200);
-    noTone(BUZ_PIN);
-    delay(80);
-  }
-  tone(BUZ_PIN, chord[l]);
-  delay(300);
-  noTone(BUZ_PIN);
-  delay(180);
-  for(m=l+1;m < 15;m++){
-    tone(BUZ_PIN, chord[m]);
-    delay(200);
-    noTone(BUZ_PIN);
-    delay(80);
-  }
-  tone(BUZ_PIN, chord[m]);
-  delay(300);
-  noTone(BUZ_PIN);
-  delay(180);
-  tone(BUZ_PIN, chord[m+1]);
-  delay(300);
-  noTone(BUZ_PIN);
-  delay(180);
-  tone(BUZ_PIN, chord[m+2]);
-  delay(600);
-  noTone(BUZ_PIN);
-  delay(120);
-
 }
 
 void draque() {
-  int chord[60] = { G4, G4, C5, D5, E5, F5, G5, C6, B5, A5, A5, G5, Fs5, Fs5, A5, G5, E5, E4, E4, E4, E4, Fs4, G4, A4, A4, B4, C5, D5, A4, A4, C5, C5, B4, A4, G4, E5, F5, E5, D5, C5, A4, C5, D5, E5, D5, C5, C5, B4, G4, G5, E5, F5, G5, A5, A4, B4, C5, F5, E5, C5};
-  int i;
+  int bpm = 70;
+  int chord[64] = { G4, G4, C5, D5, E5, F5, G5, C6, B5, A5, A5, G5, 0, Fs5, Fs5,    
+                    A5, G5, E5, E4, E4, E4, E4, Fs4, Gs4, A4, 0, A4, B4, C5, D5, 0, A4, A4, C5, C5, B4, A4, G4, E5, F5, E5, D5, C5, A4, C5, D5, E5, D5, C5, C5, B4, G4, G5, E5, F5, G5, A5, 0, A4, B4, C5, F5, E5, C5};
+
+  int duration[64] = { EIGHTH_P/bpm, SIXTEENTH/bpm, QUARTER/bpm, QUARTER/bpm, QUARTER/bpm, QUARTER/bpm, QUARTER/bpm, HALF/bpm, EIGHTH_P/bpm, SIXTEENTH/bpm, QUARTER_P/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, QUARTER/bpm, HALF/bpm, EIGHTH_P/bpm, SIXTEENTH/bpm, QUARTER/bpm, QUARTER/bpm, QUARTER/bpm, QUARTER/bpm, HALF/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, HALF/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, QUARTER/bpm, QUARTER/bpm, QUARTER/bpm, QUARTER/bpm, HALF/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, HALF/bpm, QUARTER/bpm, QUARTER/bpm, HALF/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, HALF/bpm, QUARTER/bpm, QUARTER/bpm, HALF/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, HALF/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, HALF/bpm, HALF/bpm, HALF/bpm};
+
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print(songlist[2]);
-  tone(BUZ_PIN, chord[0]);
-  delay(300);
-  noTone(BUZ_PIN);
-  delay(100);
-  tone(BUZ_PIN, chord[1]);
-  delay(200);
-  noTone(BUZ_PIN);
-  delay(50);
-  for(i=2;i < 7;i++){
-    tone(BUZ_PIN, chord[i]);
-    delay(500);
-    noTone(BUZ_PIN);
-    delay(100);
+  for(int i=0;i < 64;i++){
+    if(chord[i] == 0){
+      noTone(BUZ_PIN);
+      delay(duration[i]);
+    }else{
+      tone(BUZ_PIN, chord[i]);
+      delay(duration[i]);
+      noTone(BUZ_PIN);
+      delay(duration[i]/5);
+    }
   }
-  tone(BUZ_PIN, chord[i]);
-  delay(1000);
   noTone(BUZ_PIN);
-  delay(200);
-  tone(BUZ_PIN, chord[i+1]);
-  delay(300);
-  noTone(BUZ_PIN);
-  delay(100);
-  tone(BUZ_PIN, chord[i+2]);
-  delay(100);
-  noTone(BUZ_PIN);
-  delay(100);
-  tone(BUZ_PIN, chord[i+3]);
-  delay(800);
-  noTone(BUZ_PIN);
-  delay(200);
-  tone(BUZ_PIN, chord[i+4]);
-  delay(200);
-  noTone(BUZ_PIN);
-  delay(400);
-  for(i=12;i<16;i++){
-    tone(BUZ_PIN, chord[i]);
-    delay(200);
-    noTone(BUZ_PIN);
-    delay(100);
-  }
 }
 
 void zelda(){
-  int chord[30] = {As4, F4, As4, As4, C5, D5, Ds5, F5, F5, F5, Fs5, Gs5, As5, As5, As5, Gs5, Fs5, Gs5, Fs5, F5};
-  int i;
+  int bpm = 130;
+  int chord[151] = {A4, 0, A4, A4, A4, A4, A4, G4, A4, 0, A4, A4, A4, A4, A4, G4, A4, 0, A4, A4, A4, A4, A4, E4, E4, E4, E4, E4, E4, E4, E4, E4, E4, A4, E4, A4, A4, B4, Cs5, D5, E5, 0, E5, E5, F5, G5, A5, 0, A5, A5, G5, F5, G5, F5, E5, E5, D5, D5, E5, F5, E5, D5, C5, C5, D5, E5, D5, C5, B4, B4, Cs5, Ds5, Fs5, E5, B4, B4, C5, D5, G5, F5, F4, F4, F4, F4, F4, F4, F4, F4, F4, F4, B4, F4, B4, B4, C5, D5, E5, F5, F5, F5, G5, A5, B5, C6, B5, A5, F5, G5, B5, A5, F5, F5, G5, B5, A5, F5, D5, E5, G5, F5, D5, B4, C5, C5, D5, E5, G5, F5, F4, F4, F4, F4, F4, F4, F4, F4, F4, F4};
+  int duration[151] = {HALF/bpm, QUARTER/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, QUARTER/bpm, EIGHTH/bpm, QUARTER/bpm, QUARTER/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, QUARTER/bpm, EIGHTH/bpm, QUARTER/bpm, QUARTER/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, EIGHTH/bpm, SIXTEENTH/bpm, SIXTEENTH/bpm, EIGHTH/bpm, SIXTEENTH/bpm, SIXTEENTH/bpm, EIGHTH/bpm, SIXTEENTH/bpm, SIXTEENTH/bpm, EIGHTH/bpm, EIGHTH/bpm};
+
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print(songlist[3]);
-  tone(BUZ_PIN, chord[0]);
-  delay(100);
-  noTone(BUZ_PIN);
-  delay(400);
-  tone(BUZ_PIN, chord[1]);
-  delay(600);
-  noTone(BUZ_PIN);
-  delay(200);
-  for(i=2;i<7;i++){
-    tone(BUZ_PIN, chord[i]);
-    delay(100);
-    noTone(BUZ_PIN);
-    delay(50);
+  for(int i=0;i < 33;i++){
+    if(chord[i] == 0){
+      noTone(BUZ_PIN);
+      delay(duration[i]);
+    }else{
+      tone(BUZ_PIN, chord[i]);
+      delay(duration[i]);
+      noTone(BUZ_PIN);
+      delay(duration[i]/5);
+    }
   }
-  tone(BUZ_PIN, chord[i]);
-  delay(800);
-  noTone(BUZ_PIN);
-  delay(400);
-  tone(BUZ_PIN, chord[i+1]);
-  delay(100);
-  noTone(BUZ_PIN);
-  delay(50);
-  tone(BUZ_PIN, chord[i+2]);
-  delay(100);
-  noTone(BUZ_PIN);
-  delay(50);
-  tone(BUZ_PIN, chord[i+3]);
-  delay(100);
-  noTone(BUZ_PIN);
-  delay(50);
-  tone(BUZ_PIN, chord[i+4]);
-  delay(100);
-  noTone(BUZ_PIN);
-  delay(50);
-  tone(BUZ_PIN, chord[i+5]);
-  delay(800);
-  noTone(BUZ_PIN);
-  delay(200);
-  tone(BUZ_PIN, chord[i+6]);
-  delay(100);
-  noTone(BUZ_PIN);
-  delay(50);
-  tone(BUZ_PIN, chord[i+7]);
-  delay(100);
-  noTone(BUZ_PIN);
-  delay(50);
-  tone(BUZ_PIN, chord[i+8]);
-  delay(100);
-  noTone(BUZ_PIN);
-  delay(50);
-  tone(BUZ_PIN, chord[i+9]);
-  delay(100);
-  noTone(BUZ_PIN);
-  delay(50);
-  tone(BUZ_PIN, chord[i+10]);
-  delay(100);
-  noTone(BUZ_PIN);
-  delay(200);
-  tone(BUZ_PIN, chord[i+11]);
-  delay(100);
-  noTone(BUZ_PIN);
-  delay(50);
-  tone(BUZ_PIN, chord[i+12]);
-  delay(800);
   noTone(BUZ_PIN);
 }
 
@@ -316,20 +188,20 @@ void showValidInput2(){
   delay(2000);
   lcd.clear();
   lcd.print("A:return home");
-  delay(2000);
+  // delay(2000);
 }
 
 void showPlaylist(){
   char key;
   int index=1;
   showValidInput1();
-  delay(2000);
+  // delay(2000);
   lcd.clear();
   lcd.print("0: Family Mart");
   lcd.setCursor(0,1);
   lcd.print("1: Noritz");
   key = keypad.waitForKey();
-  while(key == '#' || key == '*'){
+  while(key == '#' || key == '*' || key == '0'){
     if(key == '#'){
       if(index <= NUM_SONGS-1){
         lcd.clear();
@@ -351,7 +223,7 @@ void showPlaylist(){
         showValidInput1();
         key = keypad.waitForKey();
       }
-    }else{
+    }else if(key == '*'){
       if(index < 2){
         showValidInput1();
         index = 0;
@@ -368,6 +240,9 @@ void showPlaylist(){
         index--;
         key = keypad.waitForKey();
       }
+    }else{
+      songs[index-1]();
+      showPlaylist();
     }
   }
   if(key == 'A'){
@@ -380,7 +255,7 @@ void showPlaylist(){
 void inputSongNum(){
   char key;
   int songNum=0, cnt=0, keyInt;
-  showValidInput2();
+  // showValidInput2();
   lcd.clear();
   lcd.print("Enter songNum");
   key = keypad.waitForKey();
@@ -452,22 +327,4 @@ void loop() {
   }else{
     return;
   }
-  /*
-  if(key >= '0' && key <='9'){
-    keyInt = key - '0';
-    if (keyInt < NUM_SONGS){
-      songs[keyInt]();
-    } else {
-      lcd.clear();
-      lcd.print("invalid input.");
-      delay(1000);
-      return;
-    }
-  }else{
-    lcd.clear();
-    lcd.print("invalid input.");
-    delay(1000);
-    return;
-  }
-  */
 }
